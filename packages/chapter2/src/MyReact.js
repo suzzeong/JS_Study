456dad5e110d47820ae0eb9b3f2df7048207f776
suzzeong <suzzeong@suz-MacBook-Pro.local>
2024-03-23 15:03:59 +0900

    [Feat] Chaper2. 과제 C 추가

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -2,10 +2,36 @@ import { createHooks } from "./hooks";
 import { render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  const renderContext = {
+    $root: null,
+    rootComponent: null,
+    currentNode: null,
+    beforeNode: null,
+  };
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  const _render = () => {
+    const currentNode = renderContext.rootComponent();
+
+    updateElement(renderContext.$root, currentNode, renderContext.currentNode);
+    resetHookContext();
+
+    renderContext.beforeNode = renderContext.currentNode;
+    renderContext.currentNode = currentNode;
+  };
+
+  function render($root, rootComponent) {
+    renderContext.$root = $root;
+    renderContext.rootComponent = rootComponent;
+    renderContext.currentNode = null;
+    renderContext.beforeNode = null;
+    _render();
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
